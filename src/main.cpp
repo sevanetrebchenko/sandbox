67abc2aec2e50fb0151251885cd231f75bdc4841
seva.netrebchenko <seva.netrebchenko@gmail.com>
2021-02-28 02:54:59 -0800

    Base window creation with all dependencies initialized.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,95 @@
 
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    int initializationCode = glfwInit();
+
+    // Failed to initialize GLFW.
+    if (!initializationCode) {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return 1;
+    }
+
+    // Setting up OpenGL properties
+    glfwWindowHint(GLFW_SAMPLES, 1); // change for anti-aliasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "ASCII", nullptr, nullptr);
+
+    // Failed to create GLFW window.
+    if (!window) {
+        std::cerr << "Failed to create GLFW window." << std::endl;
+        return 1;
+    }
+
+    // Initialize OpenGL.
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        throw std::runtime_error("Failed to initialize Glad (OpenGL).");
+    }
+
+    // OpenGL properties.
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/inconsolata/Inconsolata-Regular.ttf", 17.0f);
+    ImGui::StyleColorsDark();
+
+    // Initialize ImGui Flags.
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    // Setup Platform/Renderer backend.
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+
+    // Set up rendering to texture via custom FBO.
+
+
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && (glfwWindowShouldClose(window) == 0)) {
+        // Start new ImGui frame.
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Enable window docking.
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+        ImGui::Begin("Overview", nullptr);
+        ImGui::Text("Render time:");
+        ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+
+
+
+        // End frame.
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup.
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
