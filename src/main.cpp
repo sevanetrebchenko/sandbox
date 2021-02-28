b85259cfb17fad2faadd688e22fe471659207bc1
seva.netrebchenko <seva.netrebchenko@gmail.com>
2021-02-28 03:29:26 -0800

    Set up basic deferred rendering setup.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
@@ -21,7 +22,10 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "ASCII", nullptr, nullptr);
+    int windowWidth = 1920u;
+    int windowHeight = 1080u;
+
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "ASCII", nullptr, nullptr);
 
     // Failed to create GLFW window.
     if (!window) {
@@ -63,7 +67,41 @@ int main() {
 
 
     // Set up rendering to texture via custom FBO.
+    GLuint FBO;
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 
+    int contentWidth = 2560;
+    int contentHeight = 1440;
+
+    // One texture attachment for scene output.
+    GLuint textureAttachment;
+    glGenTextures(1, &textureAttachment);
+
+    glBindTexture(GL_TEXTURE_2D, textureAttachment);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, contentWidth, contentHeight, 0, GL_RGBA, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureAttachment, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // Renderbuffer for depth information.
+    GLuint RBO;
+    glGenRenderbuffers(1, &RBO);
+
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, contentWidth, contentHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, RBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        std::cerr << "Custom FBO incomplete." << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && (glfwWindowShouldClose(window) == 0)) {
         // Start new ImGui frame.
@@ -71,14 +109,32 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // Scene rendering.
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
         // Enable window docking.
         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-        ImGui::Begin("Overview", nullptr);
-        ImGui::Text("Render time:");
-        ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        if (ImGui::Begin("Overview", nullptr)) {
+            ImGui::Text("Render time:");
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
         ImGui::End();
 
+        // Render output texture via ImGui.
+        if (ImGui::Begin("Scene Output", nullptr)) {
+            // Ensure proper scene image scaling.
+            float maxWidth = ImGui::GetWindowContentRegionWidth();
+            float aspect = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
+            ImVec2 imageSize = ImVec2(maxWidth, maxWidth / aspect);
 
+            ImGui::Image(reinterpret_cast<ImTextureID>(textureAttachment), imageSize, ImVec2(0, 1), ImVec2(1, 0));
+        }
+        ImGui::End();
 
         // End frame.
         ImGui::Render();
